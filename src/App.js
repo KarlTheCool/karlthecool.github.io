a497dab176249a81a987c198e448d79533149a4d
Karl Phillips <k@rlphilli.ps>
2021-02-06 03:21:48 -0800

    Yarn lol (#3)
    * haha goodbye
    
    * Auto deploy static react app

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://cdn.frankerfacez.com/emoticon/421124/4" className="App-logo" alt="logo" />
+        <p>
+          Welcome to the Karl Zone
+        </p>
+        <a
+          className="App-link"
+          href="https://twitter.com/KarlTheCool"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Check me out on Twitter
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
